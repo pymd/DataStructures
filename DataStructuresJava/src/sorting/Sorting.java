2cf904fdc3bb7a22f55dd1d6c397c093ce5d27ef
Piyush <piyush@flocare.health>
2019-05-10 12:05:43 +0530

    Java: added counting sort

--- DataStructuresJava/src/sorting/Sorting.java
+++ DataStructuresJava/src/sorting/Sorting.java
@@ -1,5 +1,6 @@
 package sorting;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import heap.MaxHeap;
 
@@ -42,6 +43,7 @@ public class Sorting {
             arr[i++] = temp[k++];
     }
 
+    // O(nlogn)
     public static void mergeSort(Integer[] arr, Integer start, Integer end){
         if(start >= end)
             return;
@@ -51,21 +53,55 @@ public class Sorting {
         merge(arr, start, mid, end);                // O(n)
     }
 
+    // O(nlogn)
     public static void heapSort(Integer[] arr, Integer n){
         MaxHeap heap = new MaxHeap(n);
         heap.setArr(arr);
         Integer[] heapArr = heap.getArr();
 
+        // O(n)
         heap.buildMaxHeap();
 
+        // O(nlogn)
         for(int i=0; i<arr.length; i++){
             Integer temp = heapArr[n-1];
             heapArr[n-1] = heapArr[0];
             heapArr[0] = temp;
 
             heap.setHeapSize(--n);
-            heap.heapify(0);
+            heap.heapify(0);            // O(logn)
         }
+    }
+
+    // O(n+k) : sorts number between range 0 to k-1
+    public static void countingSort(Integer[] arr, Integer k){
+        int n = arr.length;
+
+        int[] output = new int[n];
+
+        // define count array                   O(k)
+        int count[] = new int[k];
+        for(int i=0; i<k; i++)
+            count[i] = 0;
+
+        // set counts                           O(n)
+        for(int i=0; i<n; i++)
+            count[arr[i]]++;
+
+        // modify count array to contain sum of previous counts         O(k)
+        for(int i=1; i<k; i++){
+            count[i] = count[i]+count[i-1];
+        }
+
+        // set output                           O(n)
+        for(int i=0; i<n; i++){
+            output[count[arr[i]]-1] = arr[i];
+            count[arr[i]]--;
+        }
+
+        // O(n)
+        for(int i=0; i<n; i++)
+            arr[i] = output[i];
     }
 
     public static void main(String[] args){
@@ -84,5 +120,10 @@ public class Sorting {
         System.out.println("Unsorted Array is: " + Arrays.toString(arr));
         heapSort(arr, arr.length);
         System.out.println("Array after heapSort is: " + Arrays.toString(arr));
+
+        arr = new Integer[]{30,12,51,21,5,1,5,55,60};
+        System.out.println("Unsorted Array is: " + Arrays.toString(arr));
+        countingSort(arr, 61);
+        System.out.println("Array after countingSort is: " + Arrays.toString(arr));
     }
 }
