551dd305a54acbbd344eeac55113197b6d483f44
Piyush <piyush@flocare.health>
2019-05-12 17:31:10 +0530

    Java: added InOrderSuccessor for BinaryTree

--- DataStructuresJava/src/tree/InOrderSuccessor.java
+++ DataStructuresJava/src/tree/InOrderSuccessor.java
@@ -1,29 +1,75 @@
 package tree;
 
 public class InOrderSuccessor {
-    Node root;
+    private Node root;
 
-    Node getRoot(){
+    public Node getRoot(){
         return root;
     }
 
-    void setRoot(Node root){
+    public void setRoot(Node root){
         this.root = root;
     }
 
-    void getInOrderSuccessor(Node root, Node node){
-        if(root == null){
-            return;
+    private Node successor = null;
+
+    private boolean setInOrderSuccessor(Node root, Node node){
+        if(root == null || node == null)
+            return false;
+
+        if(root.getLeftChild() == node){
+            successor = root;
+            return true;
         }
-        if(node == null){
-            System.out.println("Node is null");
-            return;
+        if(root.getRightChild() == node)
+            return true;
+
+        // search node in LST
+        successor = root;
+        if(setInOrderSuccessor(root.getLeftChild(), node))
+            return true;
+
+        // search node in RST
+        successor = null;
+        return setInOrderSuccessor(root.getRightChild(), node);
+    }
+
+    private Node getLeftMostNode(Node root){
+        if(root == null)
+            return null;
+        while(root.getLeftChild() != null){
+            root = root.getLeftChild();
         }
-        getInOrderSuccessor(root.getLeftChild(), node);
-        if(root.getKey() == node.getKey()){
-            //            return node;
+        return root;
+    }
+
+    public Node getInOrderSuccessor(Node root, Node node){
+        if(root == null || node == null)
+            return null;
+        if(node.getRightChild() != null){
+            return getLeftMostNode(node.getRightChild());
+        } else {
+            setInOrderSuccessor(root, node);
+            return successor;
         }
-        getInOrderSuccessor(root.getRightChild(), node);
+    }
+
+    private Node prev = null;
+    public Node successorUsingReverseInOrder(Node root, Node searchNode){
+        if(root == null || searchNode == null)
+            return null;
+
+        Node result = successorUsingReverseInOrder(root.getRightChild(), searchNode);
+        if(result != null)
+            return result;
+
+        if(root == searchNode){
+            return prev;
+        }else{
+            prev = root;
+        }
+
+        return successorUsingReverseInOrder(root.getLeftChild(), searchNode);
     }
 
     public static void main(String[] args){
@@ -31,6 +77,19 @@ public class InOrderSuccessor {
         tree.setRoot(new Node(1));
         tree.getRoot().setLeftChild(new Node(2));
         tree.getRoot().setRightChild(new Node(3));
+        tree.getRoot().getLeftChild().setLeftChild(new Node(4));
+        tree.getRoot().getLeftChild().setRightChild(new Node(5));
+        tree.getRoot().getLeftChild().getLeftChild().setRightChild(new Node(6));
 
+        Node n = tree.getRoot().getLeftChild().getLeftChild();
+        System.out.println("InOrder Successor for node: " + n.getKey() + " is: ");
+        Node successor = tree.getInOrderSuccessor(tree.getRoot(), n);
+        if(successor != null)
+            System.out.println(successor.getKey());
+
+        System.out.println("InOrder Successor for node: " + n.getKey() + " using reverseInOrder is: ");
+        Node successor2 = tree.successorUsingReverseInOrder(tree.getRoot(), n);
+        if(successor2 != null)
+            System.out.println(successor2.getKey());
     }
 }
